b751a8a88c7feb303ccf63f74e858979b1e0a68a
alienperfect <alienperfecthq@gmail.com>
2022-01-05 23:02:02 +0200

    first commit

---
+++ articles/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Article
+
+
+class ArticleEditForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['text', 'category']
