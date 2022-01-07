129eb846089380a3bcb7ce06dfd8d9fb05d6e2c6
alienperfect <alienperfecthq@gmail.com>
2022-01-07 19:48:28 +0200

    initial commit

--- articles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ArticlesView, ArticleCreateView, ArticleEditView
-
-app_name = 'articles'
-urlpatterns = [
-    path('', ArticlesView.as_view(), name='articles'),
-    path('create/', ArticleCreateView.as_view(), name='create'),
-    path('edit/<title>/version=<int:version>', ArticleEditView.as_view(), name='edit')
-]
--- categories/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CategoriesView
-
-app_name = 'categories'
-urlpatterns = [
-    path('', CategoriesView.as_view(), name='categories')
-]
